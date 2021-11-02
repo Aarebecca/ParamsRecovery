52c4a9f875c8fba8c1668981df07bc75a383fb81
Aaron <943720372@qq.com>
2021-11-03 00:08:21 +0800

    refactor(ast): 更新了 AST 方法

--- data/preprocess/src/ast.ts
+++ data/preprocess/src/ast.ts
@@ -1,62 +1,91 @@
-import type * as ESTree from "estree";
-const esprima = require("esprima");
+import generate from "@babel/generator";
+import { each, isObject } from "@antv/util";
+import { isFunctionAvailable } from "./drop";
+import { parse } from "@babel/parser";
+/**
+ * 抽象语法树 ast 的解析及函数定义提取
+ */
+import type { FunctionNode } from "./types";
+import type {
+  File as AST_TYPE,
+  StringLiteral,
+  Node,
+  FunctionExpression,
+  ExpressionStatement,
+} from "@babel/types";
 
-export type FunctionNode =
-  | ESTree.FunctionDeclaration
-  | ESTree.FunctionExpression
-  | ESTree.ArrowFunctionExpression
-  | ESTree.NewExpression;
-class AST {
-  private ast: ESTree.Program;
-  constructor(code: string | ESTree.Program) {
-    if (typeof code === "string") {
-      this.ast = esprima.parseScript(code);
-    } else {
-      this.ast = code;
-    }
-  }
+export class AST {
+  public ast: AST_TYPE;
 
-  public getAST(): ESTree.Program {
-    return this.ast;
+  constructor(code: string | AST_TYPE) {
+    this.ast = typeof code === "string" ? AST.parse(code) : code;
   }
 
   /**
-   * 从 ast 中抽取出所有的函数
-   * 包括函数声明、函数表达式、箭头函数
-   * @returns {FunctionNode[]}
+   * 解析代码为 ast
    */
-  public extractFunction(): FunctionNode[] {
-    const functions: FunctionNode[] = [];
-    const walk = (node: ESTree.Node) => {
-      if (
-        [
-          "FunctionDeclaration",
-          "FunctionExpression",
-          "ArrowFunctionExpression",
-        ].includes(node.type)
-      ) {
-        functions.push(node as FunctionNode);
-      } else if (node.type === "Program") {
-        node.body.forEach(walk);
-      } else if (node.type === "BlockStatement") {
-        node.body.forEach(walk);
-      } else if (node.type === "ExpressionStatement") {
-        walk(node.expression);
-      } else if (node.type === "CallExpression") {
-        walk(node.callee);
-        node.arguments.forEach(walk);
-      } else if (node.type === "MemberExpression") {
-        walk(node.object);
-        walk(node.property);
-      } else if (
-        node.type === "NewExpression" &&
-        node.callee.type === "Identifier" &&
-        node.callee.name === "Function"
-      ) {
-        functions.push(node);
-      }
+  public static parse(code: string): AST_TYPE {
+    try {
+      return parse(code);
+    } catch (es) {
+      throw new Error(`parse error: ${es}`);
+    }
+  }
+
+  public static generate(code: AST_TYPE) {
+    return generate(code).code;
+  }
+
+  /**
+   * 获得 ast 中所有的函数定义节点
+   * 包括函数声明、函数表达式、箭头函数
+   */
+  public get functions(): FunctionNode[] {
+    const funcs: FunctionNode[] = [];
+    const walk = (node: Node) => {
+      each(node, (val: Node, key: string) => {
+        if (isObject(val)) {
+          if (
+            [
+              "FunctionDeclaration",
+              "FunctionExpression",
+              "ArrowFunctionExpression",
+            ].includes(val.type)
+          ) {
+            funcs.push(val as FunctionNode);
+          } else if (
+            val.type === "NewExpression" &&
+            val.callee.type === "Identifier" &&
+            val.callee.name === "Function"
+          ) {
+            /**
+             * 如果使用 new Function 来构造函数
+             * 将其转化为 FunctionExpression
+             */
+            // 构造 function
+            const params = val.arguments
+              .slice(0, -1)
+              .map((arg) => (arg as StringLiteral).value);
+            const f = `(function (${params.join(",")}){ ${
+              (val.arguments.slice(-1)[0] as StringLiteral).value
+            } })`;
+            funcs.push(
+              (AST.parse(f).program.body[0] as ExpressionStatement)
+                .expression as FunctionExpression
+            );
+          }
+          walk(val);
+        }
+      });
     };
-    walk(this.ast);
-    return functions;
+    walk(this.ast.program);
+    return funcs;
+  }
+
+  /**
+   * 获得可用的function
+   */
+  public get availableFunctions() {
+    return this.functions.filter(isFunctionAvailable);
   }
 }
