589ec019f0bb1a0a5be93ac314933e63352a94ce
Aaron <943720372@qq.com>
2021-11-03 15:34:03 +0800

    feat(drop): 实现了 drop 中的方法

--- data/preprocess/src/drop.ts
+++ data/preprocess/src/drop.ts
@@ -2,39 +2,49 @@
  * 判断给定方法是否可用
  */
 import type { FunctionNode } from "types";
-import { flatten } from "@antv/util";
-import { extractVariableNames } from "./extract";
+import { extractVariableNamesList, extractArgumentNamesList } from "./extract";
 
 /**
  * 方法是否被压缩
+ *
+ * 主要是判断参数名、方法名长度是否为1
+ *
+ * ratio 和 minLen 表示，如果超过 ratio 比例的变量名、参数名的长度都大于 minLen，则认为是未压缩的
  */
-function isConfounding(f: FunctionNode): boolean {
-  /**
-   * 主要是判断参数名、方法名长度是否为1
-   */
-  const variableNames = extractVariableNames(f);
-  console.log(variableNames);
+export function isConfounding(
+  f: FunctionNode,
+  ratio = 0.8,
+  minLen = 2
+): boolean {
+  const variableNamesList = extractVariableNamesList(f);
+  const argumentNamesList = extractArgumentNamesList(f);
+  const list = [...argumentNamesList, ...variableNamesList];
 
-  return true;
+  return list.filter((v) => {
+    return (v.slice(0, 3) === "..." ? v.slice(3) : v).length >= minLen;
+  }).length >=
+    list.length * ratio
+    ? false
+    : true;
 }
 
 /**
  * 方法中是否包含参数
  */
-function hasParameter(f: FunctionNode): boolean {
-  return true;
+export function hasParameter(f: FunctionNode): boolean {
+  return extractArgumentNamesList(f).length > 0;
 }
 
 /**
  * 方法中是否包含变量
  */
-function hasVariable(f: FunctionNode): boolean {
-  return true;
+export function hasVariable(f: FunctionNode): boolean {
+  return extractVariableNamesList(f).length > 0;
 }
 
 /**
  * 方法是否可用
  */
 export function isFunctionAvailable(f: FunctionNode): boolean {
-  return isConfounding(f) && (hasParameter(f) || hasVariable(f));
+  return !isConfounding(f);
 }
