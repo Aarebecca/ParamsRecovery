65b79f3278853be99e75abf627f59c298cadaa34
Aaron <943720372@qq.com>
2021-11-09 10:59:16 +0800

    feat(uglify): 支持导出压缩混淆代码数据

--- data/preprocess/src/index.ts
+++ data/preprocess/src/index.ts
@@ -1,16 +1,27 @@
+import * as process from "process";
+import colors from "colors";
+import run from "./run";
+import { createDataset } from "./uglify";
 export * from "./ast";
 export * from "./drop";
 export * from "./extract";
 export * from "./uglify";
 
-import run from "./run";
-import * as process from "process";
-
 const v8 = require("v8");
 console.log(
   "可用堆大小: ",
   (v8.getHeapStatistics().total_available_size / 1024 / 1024 / 1024).toFixed(2),
-  " GB"
+  "GB"
 );
 
-run(...process.argv.slice(2));
+const [f, ...args] = process.argv.slice(2);
+if (f === "run") {
+  console.log(colors.gray("执行代码提取转换"));
+  // @ts-ignore
+  run(...args);
+} else if (f === "uglify") {
+  console.log(colors.gray("执行代码压缩"));
+  createDataset();
+} else {
+  console.log(colors.gray(`未选择正确的命令！, ${process.argv.slice(2)}`));
+}
--- data/preprocess/src/uglify.ts
+++ data/preprocess/src/uglify.ts
@@ -1,9 +1,47 @@
 /** 从数据中创建训练所需的数据集 */
-const fs = require("fs");
-const UglifyJS = require("uglify-js");
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import path from "path";
+import colors from "colors";
+import UglifyJS from "uglify-js";
+import { exit } from "process";
 
 export function uglify(code: string) {
   return UglifyJS.minify(code).code;
 }
 
-function createDataset() {}
+export function createDataset() {
+  const sourceDir = "../dataset/src";
+  const targetDir = "../dataset/tar";
+  const files = readdirSync(sourceDir);
+  console.log(`任务量: ${files.length}`);
+
+  for (const file of files) {
+    console.log(`开始处理: ${file}`);
+
+    const source = JSON.parse(
+      readFileSync(`${sourceDir}/${file}`, "utf8")
+    ) as string[];
+    const raw: string[] = [];
+    const minify: string[] = [];
+    source.forEach((code: string) => {
+      if (code.length < 1500 && code.length > 50) {
+        raw.push(code);
+        minify.push(uglify(code));
+      }
+    });
+    writeFileSync(
+      path.join(targetDir, "raw", file),
+      JSON.stringify(raw),
+      "utf8"
+    );
+    writeFileSync(
+      path.join(targetDir, "minify", file),
+      JSON.stringify(minify),
+      "utf8"
+    );
+    console.log(colors.green(`${file} done`));
+  }
+
+  console.log(colors.green("任务完成"));
+  exit();
+}
